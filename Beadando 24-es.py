17c76f15fac72709750653dbcaa18fa69a7a9e79
Bujdir <47773535+Bujdir@users.noreply.github.com>
2019-05-17 13:15:49 +0200

    Update Beadando 24-es.py

--- Beadando 24-es.py
+++ Beadando 24-es.py
@@ -1,19 +1,31 @@
-def convertToNewBase(sum,targetBase):
-    num=''
-    powers=[]
-    for x in reversed(range(targetBase)):
-        powers.append(2**x)
-    for i,x in enumerate(powers):
-        if sum-x>=0:
-            num+='1'
-            sum-=x
-        else:
-            num+='0'
-    print(''.join(num))
-
-num1=int(input("Add meg az első számot: "))
-num2=int(input("Add meg a második számot: "))
-sumofnums=num1+num2
-tbase=int(input("Add meg a bázist: "))
-
-print(convertToNewBase(sumofnums,tbase))
+def konvtizre(num,bazis):
+    hossz=len(str(num))
+    tsz=0
+    for i in range(hossz):
+        sz=str(num)
+        tsz+=int(num[i])*bazis**(hossz-1)
+        hossz-=1
+    return tsz
+def vissza(num,bazis):
+    index=num**0
+    hatvanye=0
+    while num>=findex:
+        index*=bazis
+        hatvanye+=1
+    index/=bazis
+    megoldas=""
+    for i in range(hatvanyertek):
+        helyiertek=0
+        while index<=num:
+            num-=index
+            helyiertek+=1
+        megoldas+=str(helyiertek)
+        index/=bazis
+    return megoldas
+def feladat(num1,num2,bazis):
+    number1=tizesse(num1,bazis)
+    number2=tizesse(num2,bazis)
+    sum=tsz1+tsz2
+    eredmeny=vissza(sum,bazis)
+    return eredmeny
+print("eredmeny:",feladat(125,149,2))
